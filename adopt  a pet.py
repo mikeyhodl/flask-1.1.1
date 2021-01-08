351edb3a294c586aef031719b5295075b2ab5b1c
Mike Owino <mike.owino40@yahoo.com>
2021-01-08 19:49:26 +0300

    Update adopt  a pet.py

--- adopt  a pet.py
+++ adopt  a pet.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from helper import pets
 
 app = Flask(__name__)
 
@@ -8,15 +9,35 @@ def index():
   <h1>Adopt a Pet!</h1>
   <p>Browse through the links below to find your new furry friend:</p>
 <ul>
-    <li>Dog</li>
-    <li>Cats</li>
-    <li>Rabits</li>
+   <li><a href="/animals/dogs">Dogs</a></li>
+  <li><a href="/animals/cats">Cats</a></li>
+  <li><a href="/animals/rabbits">Rabbits</a></li>
 </ul>
   '''
 
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
-    html = '<h1>List of pets</h1>'
+    html = f''' <h1>List of pets</h1>
+    '''
+    html += "<ul>"
+    for index, pet in enumerate(pets[pet_type]):
+        html += f'<li><a href="/animals/{pet_type}/{index}">{pet["name"]}</a></li>'
+    
+    html += "</ul>"
     return html;
 
-  
+@app.route("/animals/<pet_type>/<int:pet_id>")
+def pet(pet_type, pet_id):
+
+    pet = pets[pet_type][pet_id]
+    
+    return f''' 
+    <h1>{pet['name']}</h1>
+    <h4>Right down it's an photo of him</h4>
+    <img src="{pet['url']}" />
+    <p>{pet['description']}</p>
+    <ul>
+      <li>Age: {pet['age']}</li>
+      <li>Breed: {pet['breed']}</li>
+    </ul>
+    '''
