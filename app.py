36bb58a21bdec21b839fb058254e0d89e322931a
Mike Owino <mike.owino40@yahoo.com>
2021-01-08 19:49:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,43 @@
 from flask import Flask
+from helper import pets
 
 app = Flask(__name__)
 
-
 @app.route('/')
-@app.route('/home')
-def home():
-    return '<h1>Hello, World!</h1>'
-
-
-@app.route('/reporter/<int:reporter_id>')
-def reporter(reporter_id):
-    return f'''
-    <h2>Reporter {reporter_id} Bio</h2>
-    <a href="/">Return to home page</a>
-    '''
-@app.route('/article/<article_name>')
-def article(article_name):
-  return f'''
-  <h2>{article_name.replace('-', ' ').title()}</h2>
-  <a href='/'>Return back to home page</a>
+def index():
+  return '''
+  <h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new furry friend:</p>
+<ul>
+   <li><a href="/animals/dogs">Dogs</a></li>
+  <li><a href="/animals/cats">Cats</a></li>
+  <li><a href="/animals/rabbits">Rabbits</a></li>
+</ul>
   '''
+
+@app.route('/animals/<pet_type>')
+def animals(pet_type):
+    html = f''' <h1>List of pets</h1>
+    '''
+    html += "<ul>"
+    for index, pet in enumerate(pets[pet_type]):
+        html += f'<li><a href="/animals/{pet_type}/{index}">{pet["name"]}</a></li>'
+    
+    html += "</ul>"
+    return html;
+
+@app.route("/animals/<pet_type>/<int:pet_id>")
+def pet(pet_type, pet_id):
+
+    pet = pets[pet_type][pet_id]
+    
+    return f''' 
+    <h1>{pet['name']}</h1>
+    <h4>Right down it's an photo of him</h4>
+    <img src="{pet['url']}" />
+    <p>{pet['description']}</p>
+    <ul>
+      <li>Age: {pet['age']}</li>
+      <li>Breed: {pet['breed']}</li>
+    </ul>
+    '''
